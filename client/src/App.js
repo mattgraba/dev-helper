a438930d81cf522d9f5cb1fd5f1747119be63bdb
Matt Graba <matt.graba14@gmail.com>
2025-06-24 11:07:37 -0500

    Restructure project, set up React client and Node backend, configure OpenAI integration, and test end-to-end error analysis

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import ErrorInputForm from './components/ErrorInputForm.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <ErrorInputForm />
+    </div>
+  );
+}
+
+export default App;
