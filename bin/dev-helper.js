a31f58de7607a17da415b2750c31b246e3f98355
Matt Graba <147743463+mattgraba@users.noreply.github.com>
2026-02-02 10:40:07 -0600

    Delete bin directory

--- bin/dev-helper.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-// ^ shebang tells the system to run file with Node.js
-// 'npm link' uses this file to register 'dev-helper' as a global command
-
-import '../cli/cli.js';   // points to the actual CLI implementation
